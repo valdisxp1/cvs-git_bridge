b0490eda010f19ea56477721b83cc1b56d912aff
Valdis <valdisxp1@gmail.com>
2013-01-26 21:28:03 +0200

    refactoring,split into two parser and command

--- cvs-git_bridge/src/main/scala/com/valdis/adamsons/App.scala
+++ cvs-git_bridge/src/main/scala/com/valdis/adamsons/App.scala
@@ -1,11 +1,16 @@
 package com.valdis.adamsons
 
+import com.valdis.adamsons.commands.Init
+import com.valdis.adamsons.commands.HelpCommand
+import com.valdis.adamsons.commands.CommandParser
+import com.valdis.adamsons.commands.Command
+
 /**
  *
  */
-object App extends Command{
+object App extends CommandParser{
   val aliases = List("")
-  override val subcommads = List(InitCommand)
+  override val subcommads = List(Init)
   val usage = "no usage yet"
   val help = "ask Valdis"
   override def parse(args: List[String]) = super.parse(args) match {
