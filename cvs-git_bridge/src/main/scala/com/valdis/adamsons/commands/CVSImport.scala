aac0149f5ea3e7acea9b5210bc7bbfe3509b8c93
Valdis <valdisxp1@gmail.com>
2013-01-29 19:56:05 +0200

    added new test cases for CVSImport.scala

--- cvs-git_bridge/src/main/scala/com/valdis/adamsons/commands/CVSImport.scala
+++ cvs-git_bridge/src/main/scala/com/valdis/adamsons/commands/CVSImport.scala
@@ -4,15 +4,24 @@ import com.valdis.adamsons.utils.GitUtils
 import scala.sys.process._
 import com.valdis.adamsons.cvs.CVSRepository
 import com.valdis.adamsons.cvs.CVSFileVersion
+import com.valdis.adamsons.utils.CVSUtils
 
 object CVSImport extends CommandParser{
   case class CVSImportCommand extends Command {
     def apply = {
       val repo = GitUtils.repo;
-      val cvsrepo = CVSRepository("/cygdrive/c/cvs/cvsroot","cvstest2");
-      println(cvsrepo.getFileContents("file1.txt", CVSFileVersion("1.1.1.1")));
-      "cvs -d \"/cygdrive/c/cvs/cvsroot\" rlog cvstest2"!;
-      //"cvs -d \"/cygdrive/c/cvs/cvsroot\" co -p cvstest2/file1.txt"!;
+      val cvsrepo = CVSRepository(CVSUtils.absolutepath("test/cvsroot"),"cvstest3");
+      val commits = cvsrepo.getFileList.flatMap(_.commits)
+      println(commits);
+      val sortedcommits = commits.sortBy(_.date)
+      sortedcommits.foreach((commit)=>{
+        println(commit.author);
+        println(commit.revision);
+        println(commit.date);
+        println
+        println(commit.comment)
+        println
+      })
       1
     }
   
