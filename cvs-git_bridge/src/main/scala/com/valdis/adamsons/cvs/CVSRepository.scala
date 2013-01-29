625dc0c4a59a81f26be234931040675dc991003f
Valdis <valdisxp1@gmail.com>
2013-01-29 19:55:43 +0200

    fixed the optional lines problem

--- cvs-git_bridge/src/main/scala/com/valdis/adamsons/cvs/CVSRepository.scala
+++ cvs-git_bridge/src/main/scala/com/valdis/adamsons/cvs/CVSRepository.scala
@@ -41,7 +41,9 @@ case class CVSRepository(val cvsroot: Option[String], val module: Option[String]
         val date = CVSRepository.CVS_DATE_FORMAT.parse(params.get("date").get)
         val author = params.get("author").get
         val commitId = params.get("commitid");
-        val comment = lines.drop(3).mkString("\n")
+        //need a good way to determine where commit message starts
+        val linesToDrop = if (lines(2).contains(": ")) { 3 } else { 2 }
+        val comment = lines.drop(linesToDrop).mkString("\n").trim
         val cvsCommit = CVSCommit(revision,date,author,comment,commitId)
         cvsCommit
       }) 
