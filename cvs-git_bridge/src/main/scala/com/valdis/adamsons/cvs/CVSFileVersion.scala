b43a96534f53be8363622b773784d4043047a990
Valdis <valdisxp1@gmail.com>
2013-01-29 18:25:22 +0200

    oops should use caracter instead of potential regex

--- cvs-git_bridge/src/main/scala/com/valdis/adamsons/cvs/CVSFileVersion.scala
+++ cvs-git_bridge/src/main/scala/com/valdis/adamsons/cvs/CVSFileVersion.scala
@@ -2,7 +2,7 @@ package com.valdis.adamsons.cvs
 
 case class CVSFileVersion(val list:List[Int]) {
   def this(s: String) {
-    this(s.split(".").toList.map(_.toInt));
+    this(s.split('.').toList.map(_.toInt));
   }
   override def toString = list.mkString(".")
 }
