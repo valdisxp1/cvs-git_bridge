afac5cae1bb7402b497a237bc5b4cd545298c22c
Valdis <valdisxp1@gmail.com>
2013-01-30 09:27:58 +0200

    added cvsimport to commands list

--- cvs-git_bridge/src/main/scala/com/valdis/adamsons/App.scala
+++ cvs-git_bridge/src/main/scala/com/valdis/adamsons/App.scala
@@ -4,13 +4,14 @@ import com.valdis.adamsons.commands.Init
 import com.valdis.adamsons.commands.HelpCommand
 import com.valdis.adamsons.commands.CommandParser
 import com.valdis.adamsons.commands.Command
+import com.valdis.adamsons.commands.CVSImport
 
 /**
  *
  */
 object App extends CommandParser{
   val aliases = List("")
-  override val subcommads = List(Init)
+  override val subcommads = List(Init,CVSImport)
   val usage = "no usage yet"
   val help = "ask Valdis"
   override def parse(args: List[String]) = super.parse(args) match {
