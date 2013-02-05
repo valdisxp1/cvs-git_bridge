e67c74f395ef7940439925cfcca5e16062f74522
Valdis <valdisxp1@gmail.com>
2013-02-05 16:27:27 +0200

    update demo script

--- cvs-git_bridge/demo.bat
+++ cvs-git_bridge/demo.bat
@@ -1,4 +1,4 @@
 java -jar cvs-git_bridge-1.0-SNAPSHOT.jar init
-java -jar cvs-git_bridge-1.0-SNAPSHOT.jar cvsimport
+java -jar cvs-git_bridge-1.0-SNAPSHOT.jar cvsimport -d test/cvsroot cvstest3
 git clone git/ git_clone/
 pause
