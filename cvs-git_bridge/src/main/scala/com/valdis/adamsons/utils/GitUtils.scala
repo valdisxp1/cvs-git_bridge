d13d40566b59d277f9142eb52fb1519de85795de
Valdis <valdisxp1@gmail.com>
2013-02-09 22:46:18 +0200

    removed unused methods stage and commit

--- cvs-git_bridge/src/main/scala/com/valdis/adamsons/utils/GitUtils.scala
+++ cvs-git_bridge/src/main/scala/com/valdis/adamsons/utils/GitUtils.scala
@@ -20,91 +20,43 @@ import org.eclipse.jgit.lib.FileMode
 import com.sun.jndi.ldap.Obj
 
 object GitUtils {
-  val gitDir="git/";
-  val gitDateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z",Locale.UK)
+  val gitDir = "git/";
+  val gitDateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z", Locale.UK)
   lazy val repo = {
     val builder = new RepositoryBuilder();
     builder.setGitDir(new File(gitDir)).
-    readEnvironment().findGitDir().build();
-  }
-  
-  def stageFile(contents:String, path:String):String={
-    val stream = new ByteArrayInputStream(contents.getBytes("UTF-8"));
-    stageFile(stream, path)
-  }
-  
-  def stageFile(stream:InputStream, path:String):String={
-	println("path: "+path)
-    val process = Process("git hash-object -w --stdin",new File(gitDir)).#<(stream)
-    val adress = process!!;
-    println(adress)
-    //stage normal file
-    Process("git update-index --add --cacheinfo 100644 " + adress + " "+path,new File(gitDir))!!;
-    adress
+      readEnvironment().findGitDir().build();
   }
 
-  def stageFile(file: File, path: String): String = {
-    println("path: " + path)
-    val inserter = repo.newObjectInserter();
-    try {
-      val fileId = inserter.insert(Constants.OBJ_BLOB, file.length(), new FileInputStream(file))
-      
-      val treeFormatter = new TreeFormatter()
-      treeFormatter.append(path, FileMode.REGULAR_FILE, fileId)
-      val treeId = inserter.insert(treeFormatter);
-      
-      inserter.flush()
-      println("fileID:"+fileId.name);
-      println("treeID:"+treeId.name);
-      fileId.name
-    } finally {
-      inserter.release()
-    }
-  }
-  def commit(message:String,parentAdress:Option[String],name:String,email:String,date:Date):String={
-    val writeTreeProcess = Process("git write-tree",new File(gitDir))
-    val treeAdress = writeTreeProcess!!;
-    
-    val stream = new ByteArrayInputStream(message.getBytes("UTF-8"));
-    
-    val commitTreeProcess = Process("git commit-tree "+treeAdress+parentAdress.map(" -p "+_+" ").getOrElse(""),new File(gitDir),
-        ("GIT_AUTHOR_NAME" -> name),("GIT_AUTHOR_EMAIL" -> email),("GIT_AUTHOR_DATE" -> gitDateFormat.format(date))).#<(stream)
-    val commitAdress = commitTreeProcess!!;
-    commitAdress
-  }
-  def hasHeadRef(branch:String):Boolean={
+  def hasHeadRef(branch: String): Boolean = {
     // assumes bare
-    new File(gitDir+"refs/heads/"+branch).exists()
-  }
-  def getHeadRef(branch:String):Option[String]={
-    val file = new File(gitDir+"refs/heads/"+branch)
-    if (file.exists) {
-    	Some(fromFile(file).getLines.mkString)
-    } else {
-    	None
-    }
+    new File(gitDir + "refs/heads/" + branch).exists()
   }
   
-  def updateHeadRef(branch:String,address:String){
-    val file = new File(gitDir+"refs/heads/"+branch)
+  def getHeadRef(branch: String): Option[String] = {
+    val file = new File(gitDir + "refs/heads/" + branch)
+    if (file.exists) {
+      Some(fromFile(file).getLines.mkString)
+    } else {
+      None
+    }
+  }
+
+  def updateHeadRef(branch: String, address: String) {
+    val file = new File(gitDir + "refs/heads/" + branch)
     if (!file.exists) {
       file.createNewFile();
     }
-    val fileOutputStream = new FileOutputStream(file,false);
-    try{
-    	fileOutputStream.write(address.getBytes())
-    }finally{
+    val fileOutputStream = new FileOutputStream(file, false);
+    try {
+      fileOutputStream.write(address.getBytes())
+    } finally {
       fileOutputStream.close()
     }
   }
-  def commitToBranch(message:String,branch:String,name:String,email:String,date:Date):String={
-    val parentAddress = getHeadRef(branch)
-    val commitAddress = commit(message, parentAddress,name,email,date)
-    updateHeadRef(branch, commitAddress)
-    commitAddress
-  }
-  def addNote(address:String,note:String){
-    val process = Process("git notes add -m \""+note+"\" "+address,new File(gitDir))
+
+  def addNote(address: String, note: String) {
+    val process = Process("git notes add -m \"" + note + "\" " + address, new File(gitDir))
     process!!
   }
 }
--- cvs-git_bridge/src/test/scala/com/valdis/adamsons/GitUtilsTest.scala
+++ cvs-git_bridge/src/test/scala/com/valdis/adamsons/GitUtilsTest.scala
@@ -5,8 +5,4 @@ import org.junit.Assert._
 import com.valdis.adamsons.utils.GitUtils
 
 class GitUtilsTest {
-  @Test
-  def testStageFile {
-    println(GitUtils.stageFile("abcd", "test"));
-  }
 }
