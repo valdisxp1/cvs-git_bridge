365806e6fb2187cca59da5ef934dc82d5a88c3ad
Valdis <valdisxp1@gmail.com>
2013-02-09 19:06:30 +0200

    updating last time visited and added needed folders

--- cvs-git_bridge/src/main/scala/com/valdis/adamsons/commands/Init.scala
+++ cvs-git_bridge/src/main/scala/com/valdis/adamsons/commands/Init.scala
@@ -3,13 +3,16 @@ package com.valdis.adamsons.commands
 import scala.collection.immutable.List
 import scala.sys.process._
 import com.valdis.adamsons.utils.GitUtils
+import java.io.File
 
 
 object Init extends CommandParser{
   case class InitCommand extends Command{
     def apply = {
-    	val repo= GitUtils.repo;
+    	val repo = GitUtils.repo;
     	repo.create(true);
+    	new File("cache/rlog").mkdirs()
+    	new File("cache/import").mkdirs()
     0
   }
   def help = ""
--- cvs-git_bridge/src/main/scala/com/valdis/adamsons/cvs/CachedCVSRepository.scala
+++ cvs-git_bridge/src/main/scala/com/valdis/adamsons/cvs/CachedCVSRepository.scala
@@ -1,17 +1,55 @@
 package com.valdis.adamsons.cvs
 
 import java.util.Date
+import java.io.File
+import scala.io.Source._
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.io.FileOutputStream
 /**
  * caches cvs rlog to speedup incremental imports
  */
-class CachedCVSRepository extends CVSRepository{
-  def lastTimeVisited: Option[Date] = None
-  def setLastTimeVisited(date: Date) = Unit
+class CachedCVSRepository(override val cvsroot: Option[String],override val module: Option[String]) extends CVSRepository(cvsroot, module){
+  def this(cvsroot: Option[String]) = this(cvsroot, None)
+  def this() = this(None, None)
+  def this(cvsroot: String, module:String) = this(Some(cvsroot), Some(module))
+  
+  def lastTimeVisited: Option[Date] = {
+    val file = new File("cache/last_rlog")
+    if(file.exists()){
+      val dateString = fromFile(file).getLines.mkString
+      Some(CachedCVSRepository.DATE_FORMAT.parse(dateString))
+    }else{
+      None
+    }
+  }
+  def setLastTimeVisited(date: Date) = {
+    val file = new File("cache/last_rlog")
+    val dateString = CachedCVSRepository.DATE_FORMAT.format(date)
+    if (!file.exists) {
+      file.createNewFile();
+    }
+    val fileOutputStream = new FileOutputStream(file,false);
+    try{
+    	fileOutputStream.write(dateString.getBytes())
+    }finally{
+      fileOutputStream.close()
+    }
+  }
+  def getCachedFileList(start: Option[Date], end: Option[Date]): List[CVSFile] = Nil
+  
   override def getFileList(start: Option[Date], end: Option[Date]): List[CVSFile] = {
-    val cashed: List[CVSFile] = Nil
+    val cashed: List[CVSFile] = getCachedFileList(start,lastTimeVisited)
     val fetched = super.getFileList(lastTimeVisited, end)
     // add fetched records to cache
     // update last visited
+    setLastTimeVisited(fetched.flatMap(_.commits).map(_.date).max)
     fetched ::: cashed
   }
+}
+
+object CachedCVSRepository{
+  def apply() = new CachedCVSRepository();
+  def apply(cvsroot: String, module: String) = new CachedCVSRepository(cvsroot, module);
+  private val  DATE_FORMAT = new SimpleDateFormat("dd.mm.yyyy",Locale.UK)
 }
