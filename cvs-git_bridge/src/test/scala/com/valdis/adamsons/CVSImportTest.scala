da0dab1e1c8515d0023808738e0d388f26d7e538
Valdis <valdisxp1@gmail.com>
2013-02-15 19:15:08 +0200

    tracking commit count

--- cvs-git_bridge/src/test/scala/com/valdis/adamsons/CVSImportTest.scala
+++ cvs-git_bridge/src/test/scala/com/valdis/adamsons/CVSImportTest.scala
@@ -9,10 +9,19 @@ import java.io.File
 import com.valdis.adamsons.utils.FileUtils
 import com.valdis.adamsons.commands.CVSImport.CVSImportCommand
 import com.valdis.adamsons.commands.Init.InitCommand
+import org.eclipse.jgit.api.Git
+import scala.collection.JavaConversions._
 
 class CVSImportTest {
   val gitDir = new File(GitUtils.gitDir)
   val cacheDir = new File("cache/")
+  
+  def commitCount = {
+    val git = new Git(GitUtils.repo)
+    val logs = git.log().call();
+    logs.count((a) => true)
+  }
+  
   def clearDirs {
     FileUtils.deleteDir(gitDir)
     FileUtils.deleteDir(cacheDir)
@@ -25,15 +34,26 @@ class CVSImportTest {
   @Test
   def testSubdirs {
     CVSImportCommand("test/cvsroot", "cvstest1").apply
+    assertEquals(1, commitCount)
   }
   @Test
   def testSubdirs2 {
     CVSImportCommand("test/cvsroot", "cvstest2").apply
+    assertEquals(1, commitCount)
+  }
+  
+  @Test
+  def testIncremental{
+    CVSImportCommand("test/cvsroot", "cvstest2").apply
+    CVSImportCommand("test/cvsroot", "cvstest2").apply
+    assertEquals(1, commitCount)
+    
   }
 
   @Test
   def testImages {
     CVSImportCommand("test/cvsroot", "cvsimagetest1").apply
+    assertEquals(1, commitCount)
   }
 
   @After
