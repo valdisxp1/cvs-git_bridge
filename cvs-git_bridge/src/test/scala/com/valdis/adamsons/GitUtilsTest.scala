5b051ee3425676cc88091b21afbcce90aad0aa3e
Valdis <valdisxp1@gmail.com>
2013-02-15 13:30:44 +0200

    updated gitutils test

--- cvs-git_bridge/src/test/scala/com/valdis/adamsons/GitUtilsTest.scala
+++ cvs-git_bridge/src/test/scala/com/valdis/adamsons/GitUtilsTest.scala
@@ -8,17 +8,36 @@ import org.junit.Before
 import com.valdis.adamsons.commands.CVSImport.CVSImportCommand
 import java.io.File
 import com.valdis.adamsons.utils.FileUtils
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib.CommitBuilder
+import org.eclipse.jgit.revwalk.RevWalk
 
 class GitUtilsTest {
-
+  var repo:Repository = null
   @Before
   def before{
-    CVSImportCommand("test/cvsroot","cvstest2")
+    repo = GitUtils.repo
+    repo.create(true)
   }
+  
   @Test
   def testGetNoteMessage{
-    GitUtils.getHeadRef("master")
+    //create an empty commit
+    val inserter = repo.newObjectInserter();
+    val revWalk = new RevWalk(repo)
+    val commitBuilder = new CommitBuilder
+    val commitId = inserter.insert(commitBuilder)
+    inserter.flush();
     
+    //add a note
+    val note = "HAHA"
+    val git = new Git(repo)
+    git.notesAdd().setMessage(note).setObjectId(revWalk.lookupCommit(commitId))
+    
+    val noteString = GitUtils.getNoteMessage(commitId.name)
+    println (noteString)
+    assertEquals(note, noteString)
   }
   
   @After
