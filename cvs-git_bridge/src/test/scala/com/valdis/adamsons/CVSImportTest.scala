7726814e118cf8890aebe37b3ac52c161d7fa668
Valdis <valdisxp1@gmail.com>
2013-02-15 16:11:19 +0200

    added more tests, have trouble running them all at once

--- cvs-git_bridge/src/test/scala/com/valdis/adamsons/CVSImportTest.scala
+++ cvs-git_bridge/src/test/scala/com/valdis/adamsons/CVSImportTest.scala
@@ -11,24 +11,33 @@ import com.valdis.adamsons.commands.CVSImport.CVSImportCommand
 import com.valdis.adamsons.commands.Init.InitCommand
 
 class CVSImportTest {
-	def clearDirs{
-	  val gitDir = new File(GitUtils.gitDir)
-	  FileUtils.deleteDir(gitDir)
-	  val cacheDir = new File("cache/")
-	  FileUtils.deleteDir(cacheDir)
-	}
-	@Before
-	def before{
-	  clearDirs
-	  InitCommand().apply
-	}
-	@Test
-	def simpleTest{
-	  CVSImportCommand("test/cvsroot","cvstest2").apply
-	}
-  
-   @After
-  def after{
+  val gitDir = new File(GitUtils.gitDir)
+  val cacheDir = new File("cache/")
+  def clearDirs {
+    FileUtils.deleteDir(gitDir)
+    FileUtils.deleteDir(cacheDir)
+  }
+  @Before
+  def before {
+    clearDirs
+    InitCommand().apply
+  }
+  @Test
+  def testSubdirs {
+    CVSImportCommand("test/cvsroot", "cvstest1").apply
+  }
+  @Test
+  def testSubdirs2 {
+    CVSImportCommand("test/cvsroot", "cvstest2").apply
+  }
+
+  @Test
+  def testImages {
+    CVSImportCommand("test/cvsroot", "cvsimagetest1").apply
+  }
+
+  @After
+  def after {
     clearDirs
   }
 }
