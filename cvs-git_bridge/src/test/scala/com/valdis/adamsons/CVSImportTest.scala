903a95be1ba14951ac72b655e9b26bce6e0d9683
Valdis <valdisxp1@gmail.com>
2013-02-16 10:51:31 +0200

    testing files for readd

--- cvs-git_bridge/src/test/scala/com/valdis/adamsons/CVSImportTest.scala
+++ cvs-git_bridge/src/test/scala/com/valdis/adamsons/CVSImportTest.scala
@@ -3,6 +3,7 @@ package com.valdis.adamsons
 import org.junit.Test
 import org.junit.Assert._
 import com.valdis.adamsons.utils.GitUtils
+import com.valdis.adamsons.utils.GitUtils._
 import org.junit.After
 import org.junit.Before
 import java.io.File
@@ -11,15 +12,29 @@ import com.valdis.adamsons.commands.CVSImport.CVSImportCommand
 import com.valdis.adamsons.commands.Init.InitCommand
 import org.eclipse.jgit.api.Git
 import scala.collection.JavaConversions._
+import org.eclipse.jgit.treewalk.TreeWalk
 
 class CVSImportTest {
   val gitDir = new File(GitUtils.gitDir)
   val cacheDir = new File("cache/")
   
   def commitCount = {
-    val git = new Git(GitUtils.repo)
     val logs = git.log().call();
     logs.count((a) => true)
+  }
+  
+  def getFileNames = {
+    val logs = git.log().call()
+    logs.map(_.getTree()).map((tree)=>{
+      val treewalk = new TreeWalk(repo)
+      treewalk.addTree(tree)
+      //TODO make this variable a value
+      var fileNames :Set[String]=Set()
+      while(treewalk.next()){
+        fileNames = fileNames + treewalk.getPathString()
+      }
+      fileNames
+    }).toList
   }
   
   def clearDirs {
@@ -35,11 +50,13 @@ class CVSImportTest {
   def testSubdirs {
     CVSImportCommand("test/cvsroot", "cvstest1").apply
     assertEquals(34, commitCount)
+    println(getFileNames)
   }
   @Test
   def testSubdirs2 {
     CVSImportCommand("test/cvsroot", "cvstest2").apply
     assertEquals(12, commitCount)
+    println(getFileNames)
   }
   
   @Test
@@ -47,6 +64,7 @@ class CVSImportTest {
     CVSImportCommand("test/cvsroot", "cvstest2").apply
     CVSImportCommand("test/cvsroot", "cvstest2").apply
     assertEquals(12, commitCount)
+    println(getFileNames)
     
   }
 
@@ -54,18 +72,21 @@ class CVSImportTest {
   def testImages {
     CVSImportCommand("test/cvsroot", "cvsimagetest1").apply
     assertEquals(3, commitCount)
+    println(getFileNames)
   }
   
   @Test
   def testRemove {
     CVSImportCommand("test/cvsroot", "cvsdeletetest").apply
     assertEquals(5, commitCount)
+    println(getFileNames)
   }
 
   @Test
   def testReAdd {
     CVSImportCommand("test/cvsroot", "cvsreaddtest").apply
     assertEquals(4, commitCount)
+    assertEquals(List(Set("good.txt", "evil.txt"), Set("good.txt"), Set("good.txt", "evil.txt"), Set("good.txt")),getFileNames)
   }
   
   @After
