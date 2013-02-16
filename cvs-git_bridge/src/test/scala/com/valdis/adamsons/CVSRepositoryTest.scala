33d3b3df9b0ca5669354053b81ca0fb33ea25242
Valdis <valdisxp1@gmail.com>
2013-02-16 21:47:44 +0200

    updated CVSRepositoryTest.scala to use cvstest5 repo

--- cvs-git_bridge/src/test/scala/com/valdis/adamsons/CVSRepositoryTest.scala
+++ cvs-git_bridge/src/test/scala/com/valdis/adamsons/CVSRepositoryTest.scala
@@ -11,18 +11,18 @@ import com.valdis.adamsons.utils.CVSUtils
 class CVSRepositoryTest {
   @Test
   def testGetFileContents {
-	  val repo = CVSRepository(CVSUtils.absolutepath("test/cvsroot"),"cvstest2")
-	  assertEquals("1", repo.getFileContents("file1.txt", CVSFileVersion("1.1.1.1")).trim());
+	  val repo = CVSRepository(CVSUtils.absolutepath("test/cvsroot"),"cvstest5")
+	  assertEquals("1", repo.getFileContents("1.txt", CVSFileVersion("1.1")).trim());
   }
   @Test
   def testFileNameList {
-    val repo = CVSRepository(CVSUtils.absolutepath("test/cvsroot"),"cvstest2")
-    assertEquals(List("file1.txt", "file2.txt", "file3.txt", "dir/file1.txt", "dir/file2.txt", "dir/file3.txt"),repo.fileNameList);
+    val repo = CVSRepository(CVSUtils.absolutepath("test/cvsroot"),"cvstest5")
+    assertEquals(List("1.txt", "2.txt", "3.txt", "dir/1.txt", "dir/2.txt", "dir/3.txt"),repo.fileNameList);
   }
   
   @Test
   def testGetFileList {
-    val repo = CVSRepository(CVSUtils.absolutepath("test/cvsroot"),"cvstest2")
+    val repo = CVSRepository(CVSUtils.absolutepath("test/cvsroot"),"cvstest5")
     println(repo.getFileList)
     assertTrue(true);
   }
