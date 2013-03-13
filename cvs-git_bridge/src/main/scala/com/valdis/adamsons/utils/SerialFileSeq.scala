dc9c91c30c9788d66e99375e809189e4bdef3cd0
Valdis <valdisxp1@gmail.com>
2013-03-13 21:07:52 +0200

    partial implementation

--- cvs-git_bridge/src/main/scala/com/valdis/adamsons/utils/SerialFileSeq.scala
+++ cvs-git_bridge/src/main/scala/com/valdis/adamsons/utils/SerialFileSeq.scala
@@ -3,18 +3,27 @@ package com.valdis.adamsons.utils
 import scala.collection.immutable.Seq
 import scala.collection.Iterator
 import java.io.File
+import java.io.ObjectInputStream
+import java.io.FileInputStream
 
-class SerialFileSeq[A <: Serializable](val file:File) extends Seq[A] {
+class SerialFileSeq[A <: Serializable](val file: File, val length: Int) extends Seq[A] {
+
   class FileIterator extends Iterator[A] {
+    val inStream = new ObjectInputStream(new FileInputStream(file))
     private var _remaining = SerialFileSeq.this.length
     def remaining = _remaining
     def hasNext: Boolean = remaining > 0
     def next: A = {
       _remaining -= 1
-      throw new Throwable
+      inStream.readObject().asInstanceOf[A]
+    }
+
+    override protected def finalize = {
+      super.finalize
+      inStream.close();
     }
   }
-  val length = 0
+  
   def apply(idx: Int): A = {
     val iterator = this.iterator
     while ((this.length - iterator.remaining) < idx) {
