f35d13ac620692e4966e86f5c96b30a8e43f28a7
Valdis <valdisxp1@gmail.com>
2013-03-23 08:57:40 +0200

    refactored tree entry to be created outside of the traversable

--- cvs-git_bridge/src/main/scala/com/valdis/adamsons/utils/SingleTreeWalkTraversable.scala
+++ cvs-git_bridge/src/main/scala/com/valdis/adamsons/utils/SingleTreeWalkTraversable.scala
@@ -4,21 +4,12 @@ import org.eclipse.jgit.treewalk.TreeWalk
 import org.eclipse.jgit.lib.FileMode
 import org.eclipse.jgit.lib.ObjectId
 
-trait TreeEntry {
-  def pathString: String
-  def fileMode: FileMode
-  def objectId: ObjectId
-}
+case class TreeEntry(val pathString: String,val fileMode: FileMode, val objectId: ObjectId)
 
 class SingleTreeWalkTraversable(val treeWalk: TreeWalk) extends Traversable[TreeEntry] {
-  class TreeEntryImpl extends TreeEntry {
-    val pathString = treeWalk.getPathString()
-    val fileMode = treeWalk.getFileMode(0)
-    val objectId = treeWalk.getObjectId(0)
-  }
   def foreach[U](f: TreeEntry => U): Unit ={
     while(treeWalk.next()){
-    	f(new TreeEntryImpl)
+    	f(new TreeEntry(treeWalk.getPathString(),treeWalk.getFileMode(0),treeWalk.getObjectId(0)))
     }
   }
 }
