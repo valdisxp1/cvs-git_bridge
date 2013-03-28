dad16c40093d83924ba7ab9974b8d21e0abc9c49
Valdis <valdisxp1@gmail.com>
2013-03-28 13:05:00 +0200

    using multi branch test instead of old one

--- cvs-git_bridge/demo.bat
+++ cvs-git_bridge/demo.bat
@@ -1,4 +1,4 @@
 java -jar target/cvs-git_bridge-1.0-SNAPSHOT.jar init
-java -jar target/cvs-git_bridge-1.0-SNAPSHOT.jar cvsimport -d test/cvsroot cvstest3
+java -jar target/cvs-git_bridge-1.0-SNAPSHOT.jar cvsimport -d test/cvsroot multibranchtest
 git clone git/ git_clone/
 pause
