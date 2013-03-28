7c59f5448aca9fa4cc11e01feb35683550c813d2
Valdis <valdisxp1@gmail.com>
2013-03-28 12:42:14 +0200

    added diff.bat to run diff command updated demo to work in current directory setup

--- cvs-git_bridge/demo.bat
+++ cvs-git_bridge/demo.bat
@@ -1,4 +1,4 @@
-java -jar cvs-git_bridge-1.0-SNAPSHOT.jar init
-java -jar cvs-git_bridge-1.0-SNAPSHOT.jar cvsimport -d test/cvsroot cvstest3
+java -jar target/cvs-git_bridge-1.0-SNAPSHOT.jar init
+java -jar target/cvs-git_bridge-1.0-SNAPSHOT.jar cvsimport -d test/cvsroot cvstest3
 git clone git/ git_clone/
 pause
---
+++ cvs-git_bridge/diff.bat
@@ -0,0 +1,2 @@
+java -jar target/cvs-git_bridge-1.0-SNAPSHOT.jar cvsdiff master changes | tee patch.diff
+pause
