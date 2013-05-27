224982cb0eedbe8aba80c50c2071880b5d3ed1dc
Valdis <valdisxp1@gmail.com>
2013-05-27 22:42:36 +0300

    even more cleanup in comments

--- cvs-git_bridge/src/main/scala/com/valdis/adamsons/utils/SingleTreeWalkTraversable.scala
+++ cvs-git_bridge/src/main/scala/com/valdis/adamsons/utils/SingleTreeWalkTraversable.scala
@@ -5,7 +5,11 @@ import org.eclipse.jgit.lib.FileMode
 import org.eclipse.jgit.lib.ObjectId
 
 case class TreeEntry(val pathString: String,val fileMode: FileMode, val objectId: ObjectId)
-
+/**
+ * Enables Scala methods for use on tree walk.
+ * Only looks at the first tree (element 0), other trees are ignored.
+ * The state of treeWalk is changed during operation.
+ */
 class SingleTreeWalkTraversable(val treeWalk: TreeWalk) extends Traversable[TreeEntry] {
   def foreach[U](f: TreeEntry => U): Unit ={
     while(treeWalk.next()){
