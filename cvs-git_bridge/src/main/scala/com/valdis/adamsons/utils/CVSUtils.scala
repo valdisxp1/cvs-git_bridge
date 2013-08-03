3236102bb5ba5be581fc90e88296d19d9acb97e4
Valdis <valdisxp1@gmail.com>
2013-08-03 22:30:39 +0300

    added pserver support

--- cvs-git_bridge/src/main/scala/com/valdis/adamsons/utils/CVSUtils.scala
+++ cvs-git_bridge/src/main/scala/com/valdis/adamsons/utils/CVSUtils.scala
@@ -2,21 +2,24 @@ package com.valdis.adamsons.utils
 
 import java.io.File
 
-
 object CVSUtils {
-	def absolutepath(relative:String) ={
-	  val nativepath = new File(relative).getAbsolutePath();
-	  //if windows let's cygwin
-	  if(nativepath.contains('\\')){
-	    "/cygdrive/" + nativepath(0).toLower+nativepath.drop(2).map((x)=>{
-	      if(x=='\\'){
-	        '/'
-	      }else{
-	        x
-	      }
-	    })
-	  }else{
-	    nativepath
-	  }
-	  }
+  def absolutepath(relative: String) = {
+    if (relative.startsWith(":pserver:")) {
+      relative
+    } else {
+      val nativepath = new File(relative).getAbsolutePath();
+      //if windows let's cygwin
+      if (nativepath.contains('\\')) {
+        "/cygdrive/" + nativepath(0).toLower + nativepath.drop(2).map((x) => {
+          if (x == '\\') {
+            '/'
+          } else {
+            x
+          }
+        })
+      } else {
+        nativepath
+      }
+    }
+  }
 }
