d9e2bffe75725d9f169c9d9a33ba0e64a6572f04
Valdis <valdisxp1@gmail.com>
2013-08-06 20:28:24 +0300

    added lazy folding

---
+++ cvs-git_bridge/src/main/scala/com/valdis/adamsons/utils/LazyFoldsUtil.scala
@@ -0,0 +1,25 @@
+package com.valdis.adamsons.utils
+
+import scala.annotation.tailrec
+
+object LazyFoldsUtil {
+  /**
+   * To avoid saying "folder".
+   */
+  class LazyFoldinator[+A](iterable: Iterable[A]) {
+
+    /**
+     * does a partial fold left while condition is true
+     */
+    def foldLeftWhile[B](z: B)(op: (B, => A) => B)(cond: B => Boolean): B = {
+      val iterator = iterable.iterator
+      var result = z
+      while (cond(result) && iterator.hasNext) {
+        result = op(result, iterator.next)
+      }
+      result
+    }
+  }
+  
+  implicit def toLazyFoldinator[A](iterable: Iterable[A]) = new LazyFoldinator(iterable)
+}
---
+++ cvs-git_bridge/src/test/scala/com/valdis/adamsons/LazyFoldsTest.scala
@@ -0,0 +1,14 @@
+package com.valdis.adamsons
+
+import com.valdis.adamsons.utils.LazyFoldsUtil._
+import org.junit.Test
+import org.junit.Assert._
+
+class LazyFoldsTest {
+  @Test
+  def testFoldLeftWhile {
+    assertEquals(0, List[Int]().foldLeftWhile(0)(_ + _)(x => true))
+    assertEquals(40, Stream.continually(5).foldLeftWhile(0)(_ + _)(_ < 40))
+    assertEquals(35, Stream.continually(5).foldLeftWhile(0)(_ + _)(_ < 31))
+  }
+}
