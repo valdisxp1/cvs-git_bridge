deca8a5b12df597fa127a462f8edd3007501a472
Valdis <valdisxp1@gmail.com>
2013-09-27 15:22:27 +0300

    shortening arrays and caching 1.1 for CVSFileVersions

--- cvs-git_bridge/src/main/scala/com/valdis/adamsons/cvs/CVSFileVersion.scala
+++ cvs-git_bridge/src/main/scala/com/valdis/adamsons/cvs/CVSFileVersion.scala
@@ -1,9 +1,11 @@
 package com.valdis.adamsons.cvs
 
-case class CVSFileVersion(val seq: Seq[Int]) {
-  def this(s: String) {
-    this(s.split('.').map(_.toInt));
-  }
+import scala.collection.mutable.WrappedArray
+
+case class CVSFileVersion(val seq: Seq[AnyVal]) {
+  def this(array:Array[Short]) = this(wrapShortArray(array))
+  def this(array:Array[Byte]) = this(wrapByteArray(array))
+  def this(array:Array[Int]) = this(wrapIntArray(array))
   
   def isBranch = seq.dropRight(1).last == 0
   /**
@@ -20,6 +22,32 @@ case class CVSFileVersion(val seq: Seq[Int]) {
   override def toString = seq.mkString(".")
 }
 
-object CVSFileVersion{
-  def apply(s: String) = new CVSFileVersion(s);
+object CVSFileVersion {
+  val V1_1 = new CVSFileVersion(Array[Byte](1, 1))
+  def apply(s: String): CVSFileVersion = {
+    val intArray = wrapIntArray(s.split('.').map(_.toInt))
+    findCachedValue(intArray).getOrElse {
+      val max = intArray.max
+      val smallestCollection = findSmallestWrappedArray(intArray, max)
+      new CVSFileVersion(smallestCollection)
+    }
+  }
+
+  private def findCachedValue(intArray: WrappedArray[Int]) = {
+    if (intArray == V1_1.seq) {
+      Some(V1_1)
+    } else {
+      None
+    }
+  }
+  
+  private def findSmallestWrappedArray(intArray: WrappedArray[Int], max: Int) = {
+    if (max <= Byte.MaxValue) {
+        intArray.map(_.toByte)
+      } else if (max <= Short.MaxValue) {
+        intArray.map(_.toShort)
+      } else { 
+        intArray 
+      }
+  }
 }
--- cvs-git_bridge/src/test/scala/com/valdis/adamsons/CVSFileVersionTest.scala
+++ cvs-git_bridge/src/test/scala/com/valdis/adamsons/CVSFileVersionTest.scala
@@ -11,9 +11,14 @@ class CVSFileVersionTest {
       val versionStr = "1.1"
       val list = List(1,1)
       assertEquals(list,CVSFileVersion(versionStr).seq)
+      assertEquals(CVSFileVersion(list),CVSFileVersion(versionStr))
       assertEquals(versionStr,CVSFileVersion(versionStr).toString)
       assertEquals(versionStr,CVSFileVersion(list).toString)
       assertEquals(CVSFileVersion(list),CVSFileVersion(versionStr))
+      
+      //checking if it uses cached value
+      assertTrue(CVSFileVersion.V1_1 eq CVSFileVersion(versionStr))
+      assertTrue(CVSFileVersion.V1_1 eq CVSFileVersion(versionStr))
     }
     {
       val versionStr = "1.1.0.3"
@@ -32,5 +37,4 @@ class CVSFileVersionTest {
       assertEquals(CVSFileVersion(list),CVSFileVersion(versionStr))
     }
   }
-
 }
