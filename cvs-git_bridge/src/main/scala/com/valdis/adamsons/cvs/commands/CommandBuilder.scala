6609d966539309f2fc33280f7c96583fe0ca6eba
Valdis <valdisxp1@gmail.com>
2014-03-30 11:34:52 +0300

    fixed -d option

--- cvs-git_bridge/src/main/scala/com/valdis/adamsons/cvs/commands/CommandBuilder.scala
+++ cvs-git_bridge/src/main/scala/com/valdis/adamsons/cvs/commands/CommandBuilder.scala
@@ -11,7 +11,7 @@ trait CommandBuilder {
     protected def arguments: Seq[String]
     protected def filePath: Option[String]
 
-    private def prefix = cvsCommand +: cvsroot.map(r => Seq("-d", Argument.esc(r))).getOrElse(Nil)
+    private def prefix = cvsCommand +: cvsroot.map(r => Seq("-d",Argument.wsEsc(r))).getOrElse(Nil)
     private def file = Seq(Argument.esc(module.map(_ + "/").getOrElse("") + filePath.getOrElse("")))
 
     private def args = prefix ++: arguments ++: file
@@ -24,6 +24,7 @@ trait Argument {
   def toArg: Seq[String]
 }
 object Argument {
+  def wsEsc(str: String) = str.trim.replace(" ","\\ ")
   def esc(str: String) = escape(str)
-  def escape(str: String) = "\"" + str + "\""
+  def escape(str: String) = "'" + str + "'"
 }
