fe5347f55ad268ff8fc2351d5b3c5e75d0a44328
Valdis Adamsons <valdisxp1@gmail.com>
2015-08-10 18:23:56 +0300

    subcommand implementation .. attempt

--- src/main/scala/com/valdis/adamsons/commands/NewCommandParser.scala
+++ src/main/scala/com/valdis/adamsons/commands/NewCommandParser.scala
@@ -6,10 +6,23 @@ trait NewCommandParser {
   def parse(scallop: Scallop): Command
   def help: String
   def config(scallop: Scallop): Scallop
+  abstract def subCommands: Map[NewCommandParser,Seq[String]] = Map.empty
+
+  private def fullConfig(rawScallop: Scallop): Scallop = {
+    val configured = config(rawScallop.banner(help))
+    subCommands.foldLeft(configured) {
+      case (partialConfig, (parser, aliases)) =>
+        aliases.foldLeft(partialConfig) {
+          (a, alias) => a.addSubBuilder(alias, parser.fullConfig(rawScallop))
+        }
+    }
+  }
 
   def main(args: Array[String]): Unit = {
-    val scallop = config(Scallop(args).banner(help)).verify
+    val rawScallop = Scallop(args)
+    val scallop = fullConfig(rawScallop).verify
     val exitCode = parse(scallop)()
+    scallop.findSubbuilder("abc")
     if (exitCode != 0) {
       System.exit(exitCode)
     } //else implicit 0
